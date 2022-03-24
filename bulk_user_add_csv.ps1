a5be3f8e42a2e85038941a638e5a6f6a02c69316
ndmausehund <64850242+ndmausehund@users.noreply.github.com>
2022-03-24 09:38:05 -0500

    Add files via upload
    Add multiple users to a security group via the csv

---
+++ bulk_user_add_csv.ps1
@@ -0,0 +1,19 @@
+﻿# Script for bulk add users to ad groups
+#Import csv file and loads adusers in variable
+
+$Users = Import-Csv -Path "C:\ScriptOutput\nameadd.csv"
+
+#Iterate AdUsers to add user account in Group
+
+foreach($User in $Users){
+        try
+        {
+
+            Add-ADGroupMember -Identity GroupName -Members $User.User -ErrorAction Stop -Verbose
+        }
+        catch
+        {
+            Write-Host "Error while adding user to adgroup"
+        }
+
+    }

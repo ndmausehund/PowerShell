6de9fc3be817a9dece58dad8c75328d4aab35ff3
ndmausehund <64850242+ndmausehund@users.noreply.github.com>
2022-03-24 09:29:43 -0500

    Add files via upload
    clears the cache for Microsoft teams

---
+++ clear_teams_cache.ps1
@@ -0,0 +1,31 @@
+﻿# ./clear-teams.ps1
+
+# Clear MS Teams cache
+
+$challenge = Read-Host "Are you sure you want to delete the Microsoft Teams Cache (Y/N)?"
+
+if ($challenge -eq "Y"){
+    Write-Host "Stopping Teams" -ForegroundColor Yellow
+    Get-Process -ProcessName Teams | Stop-Process -Force
+    Clear
+    Write-Host "Teams Stopped, please wait..." -ForegroundColor Green
+    
+    Start-Sleep -Seconds 5
+    Write-Host "Clearing Teams Disk Cache"
+    try{
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\application cache\cache" -force | Remove-Item -Confirm:$false
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\blob_storage" -recurse -force | Remove-Item -Confirm:$false
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\cache" | Remove-Item -Confirm:$false
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\databases" -recurse -force | Remove-Item -Confirm:$false
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\gpucache" -force | Remove-Item -Confirm:$false
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\Indexeddb" -recurse -force | Remove-Item -recurse -Confirm:$false
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\Local Storage" -recurse -force | Remove-Item -recurse -Confirm:$false
+        Get-ChildItem -Path $env:APPDATA\"Microsoft\teams\tmp" -recurse -force | Remove-Item -Confirm:$false
+    }catch{
+        echo $_
+    }
+    Write-Host "Cleanup Complete." -ForegroundColor Green
+}
+# Restart Teams
+Start-Process $env:LOCALAPPDATA\Microsoft\teams\current\Teams.exe
+# To delete the cache for ALL users, change the paths above replacing $env:APPDATA with 'C:\Users\*\AppData'
